600efaae74d758ab071330b1f327852f244b7bee
jihooDev <wlgn829@kakao.com>
2024-01-31 08:58:27 +0900

    feat: first commit

--- src/App.js
+++ src/App.js
@@ -2,22 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const fetchData = () => {
+    fetch('/get_all_team').then(async (res) => {
+      const data = await res.json();
+
+      console.log(data);
+
+      return data;
+    }).catch(error => {
+      console.log(error);
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={fetchData}>
+        가져오기
+      </button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,12 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+async function enableMocking() {
+  if (process.env.NODE_ENV === 'development') {
+    const { worker } = await import('./mocks/browser')
+
+    return worker.start();
+  }
+}
+
+
+enableMocking().then(() => {
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+
+  return root.render(<App />);
+}).catch(err => console.error(err))
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
